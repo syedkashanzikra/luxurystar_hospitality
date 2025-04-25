ef118403d51b6ad45935b1bf33dd7e412393d29d
Anasashfaq40 <160334997+Anasashfaq40@users.noreply.github.com>
2025-04-25 10:07:51 +0500

    backend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { Index } from "./components/Index.jsx";
 import { About } from "./components/About.jsx";
 import { Account } from "./components/Account.jsx";
 import { Contact } from "./components/Contact.jsx";
-import { Routes, Route, BrowserRouter } from "react-router-dom"; // <- this is the fix
+import { Routes, Route, BrowserRouter } from "react-router-dom"; 
 
 function App() {
   return (
