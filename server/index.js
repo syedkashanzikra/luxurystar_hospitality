cae4efc71256ecf94e54d6de76b1536d1512579d
SyedKashanAbbasNaqvi <kashan@zikrainfotech.com>
2025-04-27 16:14:16 +0500

    Cors Issue Done
    Co-Authored-By: ayeshy-farooqi <203347859+ayeshy-farooqi@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 4000
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js'; 
+import cors from 'cors';
+import contactRoutes from './routes/contactRoutes.js';
+dotenv.config();
+connectDB();
 
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+
+
+//cors
+// Allow requests from the frontend
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  credentials: true
+}));
+
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+  res.send('Hello World!');
+});
+app.use('/api', contactRoutes);
+// Server start
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
